4a2fb6784f889f9fb9caa7b9b7c39b82408e091e
Josh Wetzel <josh.wetzel@icloud.com>
2017-07-29 23:28:37 -0400

    Update gemspec files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,6 @@
 require 'cryptosystem'
 require 'minitest/autorun'
 
-if !defined?(Minitest::Test)
-  Minitest::Test = MiniTest::Unit::TestCase
-end
-
 Cryptosystem::RSA.configure do |config|
   config.password = 'test'
   config.private_key_path = 'test/keys/rsa.key'
